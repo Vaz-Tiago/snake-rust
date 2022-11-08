4c0ef158653591f9e4dd3e2364f6eb9691713b73
Tiago Vaz <tiago.vaz@hotmail.com>
2022-11-08 20:39:42 -0300

    feat: first commit

---
+++ main.rs
@@ -0,0 +1,13 @@
+
+fn main() {
+  let message = "Hello world!";
+  let message_2 = print_welcome(message);
+  println!("{}", message_2);
+}
+
+fn print_welcome(text: &str) -> &str {
+  println!("{}", text);
+  let new_message = "Hi There";
+  // return new_message;
+  new_message
+}
