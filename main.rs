0ba44f173e728370eb2b5b040b8ec0b7db1b2475
Tiago Vaz <tiago.vaz@hotmail.com>
2022-11-08 20:55:50 -0300

    feat: primitive types

--- main.rs
+++ main.rs
@@ -1,13 +1,22 @@
 
 fn main() {
+  // primitive types
   let message = "Hello world!";
-  let message_2 = print_welcome(message);
-  println!("{}", message_2);
-}
 
-fn print_welcome(text: &str) -> &str {
-  println!("{}", text);
-  let new_message = "Hi There";
-  // return new_message;
-  new_message
+  // boolean values
+  let is_valid = true;
+
+  // 2ˆ8 - 1 = 255
+  // always the same formula
+  // positive integer only
+  let small_num:u8 = 255;
+
+  // -2ˆ7 -> 2^7 = -128 -> 127
+  // always the same formula, the number - 1
+  // i can hold negative numbers
+  let small_num_2: i8 = 127;
+
+  // defined by operate system architecture, 32 or 64 bits
+  let sys_num: isize = -10;
+  let sys_num_2: usize = 10;
 }
