a4bed5e4987946ed71d581e145abdda52489fe31
Tiago Vaz <tiago.vaz@hotmail.com>
2022-11-08 21:16:55 -0300

    feat: numbers

--- main.rs
+++ main.rs
@@ -1,22 +1,18 @@
 
 fn main() {
-  // primitive types
-  let message = "Hello world!";
+  let custom_num = 98_000;    // _ is just for formatting
+  let hex_num = 0xfa;         // hexadecimal number
+  let bin_num = 0b0010_1011;  // binary number
+  let byte_num = b'A';         // byte number
 
-  // boolean values
-  let is_valid = true;
+  println!("{}", custom_num); // 98000
+  println!("{}", hex_num);    // 250
+  println!("{}", bin_num);    // 43
+  println!("{}", byte_num);   // 65
 
-  // 2ˆ8 - 1 = 255
-  // always the same formula
-  // positive integer only
-  let small_num:u8 = 255;
+  let float_num: f32 = 3.14;
+  let float_num_2: f64 = 3.2334327489;
 
-  // -2ˆ7 -> 2^7 = -128 -> 127
-  // always the same formula, the number - 1
-  // i can hold negative numbers
-  let small_num_2: i8 = 127;
-
-  // defined by operate system architecture, 32 or 64 bits
-  let sys_num: isize = -10;
-  let sys_num_2: usize = 10;
+  println!("{}", float_num);
+  println!("{}", float_num_2);
 }
